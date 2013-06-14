80a8b509e6c424b91f1ba58303e35914dc10d64f
Justin Mayfield <mayfield@mayfield-lx.CP.local>
2013-06-14 16:18:49 -0600

    PEP8 formating (mostly).
    Added argparse to module with support for setting the host, port
    and db used for the redis connection.

--- redis_browser/browser.py
+++ redis_browser/browser.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import defaultdict
 from redis import Redis
 from flask import Flask, render_template, request, g, redirect, url_for
@@ -9,14 +10,17 @@ TYPE_TEMPLATES = {
     'hash': 'types/hash.html'
 }
 
+
 @app.before_request
 def before_request():
-    g.redis = Redis()
-    
+    g.redis = Redis(host=mainargs.host, port=mainargs.port, db=mainargs.db)
+
+
 @app.teardown_request
 def teardown_request(exception):
     if hasattr(g, 'redis'):
         g.redis.connection_pool.disconnect()
+
 
 @app.context_processor
 def keys():
@@ -26,27 +30,31 @@ def keys():
 
     return dict(keys=keys)
 
+
 @app.route('/')
-def index():    
+def index():
     data = None
     key = request.args.get('k')
-    key_type = key and g.redis.type(key) # don't call redis without a key
+    key_type = key and g.redis.type(key)  # don't call redis without a key
     if key:
-        if key_type == 'none': # bogus key, possibly bad paramenter
+        if key_type == 'none':  # bogus key, possibly bad paramenter
             return redirect(url_for('index'))
-            
+
         data = {
             'hash':   lambda x: g.redis.hgetall(x),
             'list':   lambda x: g.redis.lrange(x, 0, -1),
             'string': lambda x: [g.redis.get(x)],
             'zset':   lambda x: g.redis.zrange(x, 0, -1, withscores=True),
             'set':    lambda x: g.redis.smembers(x)
-            
         }[key_type](key)
-        
-    return render_template(TYPE_TEMPLATES.get(key_type, 'index.html'), 
-        data=data,
-        _key=key)
+
+    return render_template(TYPE_TEMPLATES.get(key_type, 'index.html'),
+                           data=data, _key=key)
 
 if __name__ == '__main__':
+    p = argparse.ArgumentParser(description='Redis Browser')
+    p.add_argument('--host', default='localhost')
+    p.add_argument('--port', type=int, default=6379)
+    p.add_argument('--db', type=int, default=0)
+    mainargs = p.parse_args()
     app.run(debug=True)
